228da26cefd76f90cac4e57cd19d875623f33a5f
Jose Obeso <40411693+JoseObeso@users.noreply.github.com>
2026-09-19 16:46:11 -0500

    Actualizar ext net archivos omitidos

---
+++ Asp_Net_SandBox_Total/AspNet_ExtNet_MVC/Models/ExtNetModel.cs
@@ -0,0 +1,8 @@
+namespace AspNet_ExtNet_MVC.Models
+{
+    public class ExtNetModel
+    {
+        public string Title { get; set; }
+        public string TextAreaEmptyText { get; set; }
+    }
+}
